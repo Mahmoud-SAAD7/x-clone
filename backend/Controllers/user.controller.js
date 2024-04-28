13a998d4f153a011be6c62a2a96e38ebff8f0309
SaadBeeh <mahmoudsaadtaha39@gmail.com>
2024-04-28 22:40:53 +0300

    x-clone

---
+++ backend/Controllers/user.controller.js
@@ -0,0 +1,162 @@
+import notificationModel from "../Models/notification.model.js";
+import userModel from "../Models/user.model.js";
+import bcrypt from "bcryptjs"
+import {v2 as cloudinary} from "cloudinary"
+ 
+export const getUserProfile = async (req, res, next) => {
+  const { userName } = req.params;
+
+  try {
+    const user = await userModel.findOne({ userName }).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const followOrUnfollowUser = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const userToFollowOrUnfollow = await userModel.findById(id);
+    const currentUser = await userModel.findById(req.user._id);
+
+    if (id === req.user._id.toString()) {
+      return res
+        .status(400)
+        .json({ error: "You can not follow or unfollow yourself" });
+    }
+
+    if (!userToFollowOrUnfollow || !currentUser) {
+      return res.status(404).json({ error: "user not found" });
+    }
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // unfollow user
+      await userModel.findByIdAndUpdate(id, {
+        $pull: { followers: req.user._id },
+      });
+      await userModel.findByIdAndUpdate(req.user._id, {
+        $pull: { following: id },
+      });
+      res.status(200).json({ message: "User unfollowed successfully" });
+    } else {
+      //follow user
+      await userModel.findByIdAndUpdate(id, {
+        $push: { followers: req.user._id },
+      });
+      await userModel.findByIdAndUpdate(req.user._id, {
+        $push: { following: id },
+      });
+
+      const newNotification = new notificationModel({
+        from: req.user._id,
+        to: userToFollowOrUnfollow._id,
+        type: "follow",
+      });
+      await newNotification.save();
+
+      res.status(200).json({ message: "User followed successfully" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getSuggestedUsers = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const usersFollowedByMe = await userModel
+      .findById(userId)
+      .select("following");
+
+    const users = await userModel.aggregate([
+      {
+        $match: {
+          _id:{ $ne :userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filteredUsers = users.filter(
+      (user) => !usersFollowedByMe.following.includes(user._id)
+    );
+
+    const suggestedUsers = filteredUsers.slice(0, 4);
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+	const { fullName, email, userName, currentPassword, newPassword, bio, link } = req.body;
+	let { profileImg, coverImg } = req.body;
+
+	const userId = req.user._id;
+
+	try {
+		let user = await userModel.findById(userId);
+		if (!user) return res.status(404).json({ message: "User not found" });
+
+		if ((!newPassword && currentPassword) || (!currentPassword && newPassword)) {
+			return res.status(400).json({ error: "Please provide both current password and new password" });
+		}
+
+		if (currentPassword && newPassword) {
+			const isMatch = await bcrypt.compare(currentPassword, user.password);
+			if (!isMatch) return res.status(400).json({ error: "Current password is incorrect" });
+			if (newPassword.length < 6) {
+				return res.status(400).json({ error: "Password must be at least 6 characters long" });
+			}
+
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(newPassword, salt);
+		}
+
+		if (profileImg) {
+			if (user.profileImg) {
+				await cloudinary.uploader.destroy(user.profileImg.split("/").pop().split(".")[0]);
+			}
+
+			const uploadedResponse = await cloudinary.uploader.upload(profileImg);
+			profileImg = uploadedResponse.secure_url;
+		}
+
+		if (coverImg) {
+			if (user.coverImg) {
+				await cloudinary.uploader.destroy(user.coverImg.split("/").pop().split(".")[0]);
+			}
+
+			const uploadedResponse = await cloudinary.uploader.upload(coverImg);
+			coverImg = uploadedResponse.secure_url;
+		}
+
+		user.fullName = fullName || user.fullName;
+		user.email = email || user.email;
+		user.userName = userName || user.userName;
+		user.bio = bio || user.bio;
+		user.link = link || user.link;
+		user.profileImg = profileImg || user.profileImg;
+		user.coverImg = coverImg || user.coverImg;
+
+		user = await user.save();
+
+		// password should be null in response
+		user.password = null;
+
+		return res.status(200).json(user);
+	} catch (error) {
+		console.log("Error in updateUser: ", error.message);
+		res.status(500).json({ error: error.message });
+	}
+};
---
+++ frontend/src/Pages/Profile/EditProfileModal.jsx
@@ -0,0 +1,159 @@
+import { useEffect, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import toast from "react-hot-toast";
+import LoadingSpinner from "../../Components/Common/LoadingSpinner";
+
+const EditProfileModal = ({authUser}) => {
+	const queryClient = useQueryClient()
+	const [formData, setFormData] = useState({
+		fullName: "",
+		userName: "",
+		email: "",
+		bio: "",
+		link: "",
+		newPassword: "",
+		currentPassword: "",
+	});
+
+	const {mutate: updateProfile , isPending:isUpdating} = useMutation({
+		mutationFn: async ()=>{
+			try {
+				const res = await fetch('/api/user/update' , {
+					method: 'POST',
+					headers:{
+						"content-type": "application/json",
+					},
+					body: JSON.stringify(formData)
+				})
+
+				const data = await res.json()
+
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong")
+				}
+
+				return data
+			} catch (error) {
+				throw new Error(error)
+			}
+		},
+		onSuccess: ()=>{
+			toast.success("Profile updated Successfully")
+			Promise.all([
+				queryClient.invalidateQueries({queryKey: ["authUser"]}),
+				queryClient.invalidateQueries({queryKey: ["userProfile"]}),
+			])
+		},
+		onError: (error)=>{
+			toast.error(error.message)
+		}
+	})
+
+	const handleInputChange = (e) => {
+		setFormData({ ...formData, [e.target.name]: e.target.value });
+	};
+
+	useEffect(()=> {
+		if (authUser) {
+			setFormData({
+				fullName: authUser.fullName,
+				userName: authUser.userName,
+				bio: authUser.bio,
+				link: authUser.link,
+				email: authUser.email,
+				newPassword: "",
+				currentPassword: "",
+			})
+		}
+	} , [authUser])
+
+	return (
+		<>
+			<button
+				className='btn btn-outline rounded-full btn-sm'
+				onClick={() => document.getElementById("edit_profile_modal").showModal()}
+			>
+				Edit profile
+			</button>
+			<dialog id='edit_profile_modal' className='modal'>
+				<div className='modal-box border rounded-md border-gray-700 shadow-md'>
+					<h3 className='font-bold text-lg my-3'>Update Profile</h3>
+					<form
+						className='flex flex-col gap-4'
+						onSubmit={(e) => {
+							e.preventDefault();
+							updateProfile();
+						}}
+					>
+						<div className='flex flex-wrap gap-2'>
+							<input
+								type='text'
+								placeholder='Full Name'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.fullName}
+								name='fullName'
+								onChange={handleInputChange}
+							/>
+							<input
+								type='text'
+								placeholder='Username'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.userName}
+								name='userName'
+								onChange={handleInputChange}
+							/>
+						</div>
+						<div className='flex flex-wrap gap-2'>
+							<input
+								type='email'
+								placeholder='Email'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.email}
+								name='email'
+								onChange={handleInputChange}
+							/>
+							<textarea
+								placeholder='Bio'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.bio}
+								name='bio'
+								onChange={handleInputChange}
+							/>
+						</div>
+						<div className='flex flex-wrap gap-2'>
+							<input
+								type='password'
+								placeholder='Current Password'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.currentPassword}
+								name='currentPassword'
+								onChange={handleInputChange}
+							/>
+							<input
+								type='password'
+								placeholder='New Password'
+								className='flex-1 input border border-gray-700 rounded p-2 input-md'
+								value={formData.newPassword}
+								name='newPassword'
+								onChange={handleInputChange}
+							/>
+						</div>
+						<input
+							type='text'
+							placeholder='Link'
+							className='flex-1 input border border-gray-700 rounded p-2 input-md'
+							value={formData.link}
+							name='link'
+							onChange={handleInputChange}
+						/>
+						<button className='btn btn-primary rounded-full btn-sm text-white'> {isUpdating ? <LoadingSpinner size="lg" /> : "Update"} </button>
+					</form>
+				</div>
+				<form method='dialog' className='modal-backdrop'>
+					<button className='outline-none'>close</button>
+				</form>
+			</dialog>
+		</>
+	);
+};
+export default EditProfileModal;
