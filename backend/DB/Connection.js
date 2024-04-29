9fa9f3aa8fd7a7b8d523c95696a28e4aa8a1c473
SaadBeeh <mahmoudsaadtaha39@gmail.com>
2024-04-29 12:40:34 +0300

    fix

--- backend/DB/Connection.js
+++ backend/DB/Connection.js
@@ -5,8 +5,8 @@ dotenv.config()
 
 const connectMongoDB = async ()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`);
+        const conn = await mongoose.connect("mongodb+srv://minaemad:minaemad@cluster0.jd2gh5w.mongodb.net/TwitterClone?retryWrites=true&w=majority&appName=Cluster0");
+        // console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connection to mongoDB: ${error.message}`);
 		process.exit(1);
